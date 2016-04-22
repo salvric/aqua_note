5bca4cc0671651195836adb3d45bc76003e25370
Salv Ric <salvatorericciu@salvo.local>
2016-04-22 18:46:49 +0200

    create a new repository for the aqua_note project

---
+++ README.md
@@ -0,0 +1,4 @@
+aqua_note
+=========
+
+A Symfony project created on April 19, 2016, 8:29 pm.
---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: salvatorericciu
+ * Date: 20/04/16
+ * Time: 19:41
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+        return $this->render('genus/show.html.twig',[
+            'name'=>$genusName,
+            'notes'=>$notes
+        ]);
+    }
+}
